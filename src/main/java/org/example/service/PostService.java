16da6089200ee7fc62128872708f0e3d2735cbb5
Maksim <madmaxim22@gmail.com>
2023-03-28 12:14:29 +0300

    fix: change removeById in PostService

--- src/main/java/org/example/service/PostService.java
+++ src/main/java/org/example/service/PostService.java
@@ -35,7 +35,7 @@ public class PostService {
     }
     
     public void removeById(long id) {
-       repository.getById(id);
+       repository.removeById(id);
     }
     
     private PostDTO convertToDTO(Post post) {
